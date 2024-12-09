8d9575b6a0b0c2f0a86127d32207a5aee0d87371
Akiyamov <akiyamov@Bakloferon>
2024-12-09 19:14:49 +0500

    Edit ps script

--- wew.ps1
+++ wew.ps1
@@ -15,7 +15,7 @@ $Register_cron = Read-Host "Register a job?
 "
 function Nekobox_files_download {
     $remote_custom_line = curl https://raw.githubusercontent.com/Akiyamov/nekobox_conf/refs/heads/main/$geoip_name.json | Select-Object -Expand Content
-    $custom_line Get-Content = "$Nekobox_dir\config\routes_box\Default" | Select-String custom | Select-Object -ExpandProperty Line
+    $custom_line = Get-Content "$Nekobox_dir\config\routes_box\Default" | Select-String custom | Select-Object -ExpandProperty Line
     $content = Get-Content "$Nekobox_dir\config\routes_box\Default"
     $content | ForEach-Object {$_ -replace $custom_line,"$remote_custom_line"} | Set-Content "$Nekobox_dir\config\routes_box\Default"
     $def_outbound = Get-Content "$Nekobox_dir\config\routes_box\Default" | Select-String def_outbound | Select-Object -ExpandProperty Line
