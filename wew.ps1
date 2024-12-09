b8e12f5900bba43060bae8ff2a79512e969516ed
Akiyamov <akiyamov@Bakloferon>
2024-12-09 17:57:41 +0500

    Edit ps script

--- wew.ps1
+++ wew.ps1
@@ -14,31 +14,37 @@ $Register_cron = Read-Host "Register a job?
 2) No
 "
 function Nekobox_files_download {
-    irm https://raw.githubusercontent.com/Akiyamov/nekobox_conf/refs/heads/master/go.mod
+    $remote_custom_line = curl https://raw.githubusercontent.com/Akiyamov/nekobox_conf/refs/heads/main/$geoip_name.json | Select-Object -Expand Content
     $custom_line Get-Content "$Nekobox_dir\config\routes_box\Default" | Select-String custom | Select-Object -ExpandProperty Line
     $content = Get-Content "$Nekobox_dir\config\routes_box\Default"
-    $content | ForEach-Object {$_ -replace $custom_line,"pear = amazing"} | Set-Content c:\temp\test.txt
-#    wget http://blog.stackexchange.com/ -OutFile "$Nekobox_dir\config\routes_box\Default"
+    $content | ForEach-Object {$_ -replace $custom_line,"$remote_custom_line"} | Set-Content "$Nekobox_dir\config\routes_box\Default"
+    $def_outbound = Get-Content "$Nekobox_dir\config\routes_box\Default" | Select-String def_outbound | Select-Object -ExpandProperty Line
+    $content = Get-Content "$Nekobox_dir\config\routes_box\Default"
+    $content | ForEach-Object {$_ -replace $def_outbound,'    "def_outbound": "bypass",'} | Set-Content "$Nekobox_dir\config\routes_box\Default"
     wget "https://github.com/$geoip_name/releases/latest/download/geoip.db" -OutFile "$Nekobox_dir\geoip.db"
     wget "https://github.com/$geoip_name/releases/latest/download/geosite.db" -OutFile "$Nekobox_dir\geosite.db"
 }
 switch ($Nekobox_geofile) {
     1 {
-        $custom_line Get-Content "$Nekobox_dir\config\routes_box\Default" | Select-String custom | Select-Object -ExpandProperty Line
         $geoip_name = "savely-krasovsky/antizapret-sing-box"
-        $action = New-ScheduledTaskAction -Execute Nekobox_refilter_download
-        $trigger = New-ScheduledTaskTrigger -AtLogon
-        $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
-        $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive
-        Register-ScheduledTask -TaskName "Nekobox route config" -Action $action -Trigger $trigger -Settings $settings -Description "Download sing-box files on startup" -Principal $principal
+        Nekobox_files_download
+        if ( $Register_cron -eq 1 ) {
+            $action = New-ScheduledTaskAction -Execute Nekobox_files_download 
+            $trigger = New-ScheduledTaskTrigger -AtLogon
+            $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
+            $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive
+            Register-ScheduledTask -TaskName "Nekobox route config" -Action $action -Trigger $trigger -Settings $settings -Description "Download sing-box files on startup" -Principal $principal
+        }
     }
     2 {
         $geoip_name = "1andrevich/Re-filter-lists"
-        $action = New-ScheduledTaskAction -Execute Nekobox_refilter_download
-        $trigger = New-ScheduledTaskTrigger -AtLogon
-        $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
-        $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive
-        Register-ScheduledTask -TaskName "Nekobox route config" -Action $action -Trigger $trigger -Settings $settings -Description "Download sing-box files on startup" -Principal $principal
+        Nekobox_files_download
+        if ( $Register_cron -eq 1 ) {
+            $action = New-ScheduledTaskAction -Execute Nekobox_files_download 
+            $trigger = New-ScheduledTaskTrigger -AtLogon
+            $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -StartWhenAvailable
+            $principal = New-ScheduledTaskPrincipal -UserId $env:USERNAME -LogonType Interactive
+            Register-ScheduledTask -TaskName "Nekobox route config" -Action $action -Trigger $trigger -Settings $settings -Description "Download sing-box files on startup" -Principal $principal
+        }
     }
 }
-Start-ScheduledTask -TaskName "Nekobox route config"
