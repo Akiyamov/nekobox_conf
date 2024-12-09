a8a7c9991af951f53ddd9a4977d79157ed48bf7e
Akiyamov <akiyamov@Bakloferon>
2024-12-09 21:22:24 +0500

    Edit config and script

--- wew.ps1
+++ wew.ps1
@@ -15,11 +15,11 @@ $Register_cron = Read-Host "Register a job?
 "
 function Nekobox_files_download {
     wget https://raw.githubusercontent.com/Akiyamov/nekobox_conf/refs/heads/main/$geoip_name.json -OutFile "$Nekobox_dir\tmp_file.json"
-    $neko_json_new = Get-Content "$Nekobox_dir\tmp_file.json" -raw | ConvertFrom-Json
+    $neko_json_new = Get-Content "$Nekobox_dir\tmp_file.json" -raw 
     $neko_json = Get-Content "$Nekobox_dir\config\routes_box\Default" -raw | ConvertFrom-Json
-    $neko_json.def_outbound | % {$_="bypass"}
-    $neko_json.custom | % {$_=$neko_json_new}
-    $neko_json | ConvertTo-Json -depth 32| set-content "$Nekobox_dir\config\routes_box\Default"
+    $neko_json.def_outbound = "bypass" 
+    $neko_json.custom = $neko_json_new
+    $neko_json | ConvertTo-Json | set-content "$Nekobox_dir\config\routes_box\Default"
     wget "https://github.com/$geoip_name/releases/latest/download/geoip.db" -OutFile "$Nekobox_dir\geoip.db"
     wget "https://github.com/$geoip_name/releases/latest/download/geosite.db" -OutFile "$Nekobox_dir\geosite.db"
 }
